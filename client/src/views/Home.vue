e37c8a9c28351b98dfb5af03591bba67da09ea27
DESKTOP-E40CQK2\Bishal <bishal.bajagain@icloud.com>
2020-10-13 17:38:59 +0900

    initial commit

---
+++ client/src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+      <div class="jumbotron" bis_skin_checked="1">
+        <h1 class="display-3">Welcome to community</h1>
+        <p class="lead">
+          A fully open source forum.
+        </p>
+        <hr class="my-4" />
+        <p>
+         This forum is all about community
+        </p>
+        <p class="lead">
+          <a class="btn btn-primary btn-lg" href="#" role="button"
+            >Sign Up</a
+          >
+        </p>
+      </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+}
+</script>
